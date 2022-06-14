748a7380b7fc1fe35953e69813b9dfa117ad820d
taylorreiter <taylorreiter@gmail.com>
2022-06-14 06:53:48 -0700

    change grab script to all at once, add genome download env

--- scripts/grab_annotation_genome_accessions.R
+++ scripts/grab_annotation_genome_accessions.R
@@ -1,19 +1,20 @@
 library(readr)
 library(dplyr)
 
-query_genomes <- read_tsv(snakemake@input[['query_genomes']])
+query_genomes <- read_csv(snakemake@input[['query_genomes']])
 lineages <- read_csv(snakemake@input[['lineages']])
 
-# grab the species of interest
-accession_w <- unlist(snakemake@wildcards[['acc']])
-query_genome_w <- query_genomes %>%
-  filter(accession == accession_w)
-
-# write a tsv file that contains the lineage information for each set of species
-# specified in the metadata file
-
-lineages_w <- lineages %>%
-  filter(species %in% query_genome_w$species) %>%
-  select(name = ident, superkingdom, phylum, class, order, family, genus, species)
-
-write_csv(lineages_w, snakemake@output[["csv"]])
+# loop over species in query_genomes and write a csv file that contains
+# all accessions for genomes of the same species in GTDB
+for(species_w in query_genomes$species){
+  # filter lineages to species of interest 
+  lineages_w <- lineages %>%
+    filter(species %in% species_w) %>%
+    select(name = ident, superkingdom, phylum, class, order, family, genus, species)
+  
+  # edit species name to contain no space
+  species_no_space_w <- gsub(" ", "_", species_w)
+ 
+  # write to csv file
+  write_csv(lineages_w, paste0("outputs/query_genomes_to_annotation_genomes/", species_no_space_w, "_gtdb_genomes.csv"))
+}
